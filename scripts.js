2e6495d5db80e7bef585c99c73ada87573fdbdec
rcrespo9 <rudycrespo@hushmail.com>
2013-12-13 13:26:52 -0500

    created visualization page

--- scripts.js
+++ scripts.js
@@ -12,127 +12,91 @@ app.controller('MyController', function ($scope, angularFire) {
 	};
 });
 
-// huffpo.json
-var messages = [ {
-  "from" : "Rudy",
-  "body" : "Awesome!"
-}, {
-  "from" : "Noel",
-  "body" : "Craziness!"
-}, {
-  "from" : "Rudy",
-  "body" : "I know, right?!"
-}, {
-  "from" : "Rudy",
-  "body" : "Incredible"
-}, {
-  "from" : "Ted",
-  "body" : "Dude."
-}, {
-  "from" : "Jordan",
-  "body" : "Amazing"
-}, {
-  "from" : "Bob",
-  "body" : "Wow..."
-}, {
-  "from" : "Jill",
-  "body" : "This is splendid."
-}, {
-  "from" : "Joe",
-  "body" : "It really is unbelievable"
-}, {
-  "from" : "Carl",
-  "body" : "Great!"
-}, {
-  "from" : "Judy",
-  "body" : "I love it."
-}, {
-  "from" : "Roy",
-  "body" : "Jesus Christ!"
-}, {
-  "from" : "Steve",
-  "body" : "Why won't this work?"
-}, {
-  "from" : "George",
-  "body" : "I'm wracking my brains here for a solution."
-}, {
-  "from" : "Tim",
-  "body" : "We're getting there!"
-} ];
+var messages = new Firebase("https://huffpochallenge.firebaseio.com/");
+
+var count = 0;
+messages.on('value', function(snapshot) {
+   snapshot.forEach(function() {
+       count++;
+   });
+});
+
 
 // d3
-var w = 1280,
-	  h = 800;
+$(setTimeout(function() {
+  var w = 1280,
+  	  h = 800;
 
-var nodes = d3.range(messages.length).map(function() { return {radius: Math.random() * 12 + 4}; }),
-    color = d3.scale.category10();
+  var nodes = d3.range(count + 1).map(function() { return {radius: Math.random() * 12 + 4}; }),
+      color = d3.scale.category10();
 
-var force = d3.layout.force()
-  .gravity(0.05)
-  .charge(function(d, i) { return i ? 0 : -2000; })
-  .nodes(nodes)
-  .size([w, h]);
+  var force = d3.layout.force()
+    .gravity(0.05)
+    .charge(function(d, i) { return i ? 0 : -2000; })
+    .nodes(nodes)
+    .size([w, h]);
 
-var root = nodes[0];
-root.radius = 0;
-root.fixed = true;
+  var root = nodes[0];
+  root.radius = 0;
+  root.fixed = true;
 
-force.start();
+  force.start();
 
-var svg = d3.select("#body").append("svg:svg")
-  .attr("width", w)
-  .attr("height", h);
+  var svg = d3.select("#body").append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
 
-svg.selectAll("circle")
-  .data(nodes.slice(1))
-.enter().append("svg:circle")
-  .attr("r", function(d) { return d.radius - 2; })
-  .style("fill", function(d, i) { return color(i % 3); });
+  svg.selectAll("circle")
+    .data(nodes.slice(1))
+  .enter().append("svg:circle")
+    .attr("r", function(d) { return d.radius - 2; })
+    .style("fill", function(d, i) { return color(i % 3); });
 
-force.on("tick", function(e) {
-var q = d3.geom.quadtree(nodes),
-    i = 0,
-    n = nodes.length;
+  force.on("tick", function(e) {
+  var q = d3.geom.quadtree(nodes),
+      i = 0,
+      n = nodes.length;
 
-while (++i < n) {
-  q.visit(collide(nodes[i]));
-}
+  while (++i < n) {
+    q.visit(collide(nodes[i]));
+  }
 
-svg.selectAll("circle")
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-});
+  svg.selectAll("circle")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+  });
 
-svg.on("mousemove", function() {
-  var p1 = d3.svg.mouse(this);
-  root.px = p1[0];
-  root.py = p1[1];
-  force.resume();
-});
+  svg.on("mousemove", function() {
+    var p1 = d3.svg.mouse(this);
+    root.px = p1[0];
+    root.py = p1[1];
+    force.resume();
+  });
 
-function collide(node) {
-  var r = node.radius + 16,
-      nx1 = node.x - r,
-      nx2 = node.x + r,
-      ny1 = node.y - r,
-      ny2 = node.y + r;
-  return function(quad, x1, y1, x2, y2) {
-    if (quad.point && (quad.point !== node)) {
-      var x = node.x - quad.point.x,
-          y = node.y - quad.point.y,
-          l = Math.sqrt(x * x + y * y),
-          r = node.radius + quad.point.radius;
-      if (l < r) {
-        l = (l - r) / l * .5;
-        node.x -= x *= l;
-        node.y -= y *= l;
-        quad.point.x += x;
-        quad.point.y += y;
+  function collide(node) {
+    var r = node.radius + 16,
+        nx1 = node.x - r,
+        nx2 = node.x + r,
+        ny1 = node.y - r,
+        ny2 = node.y + r;
+    return function(quad, x1, y1, x2, y2) {
+      if (quad.point && (quad.point !== node)) {
+        var x = node.x - quad.point.x,
+            y = node.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y),
+            r = node.radius + quad.point.radius;
+        if (l < r) {
+          l = (l - r) / l * .5;
+          node.x -= x *= l;
+          node.y -= y *= l;
+          quad.point.x += x;
+          quad.point.y += y;
+        }
       }
-    }
-    return x1 > nx2
-        || x2 < nx1
-        || y1 > ny2
-        || y2 < ny1;
+      return x1 > nx2
+          || x2 < nx1
+          || y1 > ny2
+          || y2 < ny1;
+    };
   };
-};
+}, 1000));
